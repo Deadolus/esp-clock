1bebe91edef12972974a5ca9bcfce81282cdef06
Simon Egli <simon.egli@u-blox.com>
2017-12-11 09:19:00 +0100

    Better ESP_LOGI logging

--- unittests/helpers/esp_log.h
+++ unittests/helpers/esp_log.h
@@ -1,7 +1,15 @@
 #pragma once
-#include <functional>
 
-//std::function<void(const char* tag, const char* string, ...)> ESP_LOGI = [](const char* tag, const char* string, ...) {
-auto ESP_LOGI = [](const char* tag, const char* string, ...) {
-printf("%s", tag);
+#include <functional>
+#include <stdarg.h>
+
+auto ESP_LOGI = [](const char* tag, const char* message, ...) {
+    va_list args;
+    va_start(args, message);
+    printf("%s: ", tag);
+    //printf("%s: %s\n", tag, message, args);
+    vprintf(message, args);
+    printf("\n");
+    va_end(args);
 };
+
