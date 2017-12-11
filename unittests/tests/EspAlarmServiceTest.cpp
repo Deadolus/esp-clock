eb3241f76f721f3e587e8aa68f8089182d8512dc
Simon Egli <simon.egli@u-blox.com>
2017-12-11 09:25:58 +0100

    Adding more AlarmService tests

--- unittests/tests/EspAlarmServiceTest.cpp
+++ unittests/tests/EspAlarmServiceTest.cpp
@@ -10,17 +10,36 @@ class EspAlarmServiceTest : public ::testing::Test {
         }
     protected:
         virtual void SetUp() {
+            alarms.getAlarms().clear();
         }
         EspAlarmService testee;
         EspAlarm alarms;
 };
 
-TEST_F(EspAlarmServiceTest, can_snooze_alarm) {
+TEST_F(EspAlarmServiceTest, can_pacify_alarm) {
     alarms_t alarm;
     alarm.status = AlarmStatus::Ringing;
     alarms.setAlarm(alarm);
     ASSERT_EQ(AlarmStatus::Ringing, alarms.getAlarms().front().status);
     testee.pacify();
     EXPECT_EQ(AlarmStatus::Pacified, alarms.getAlarms().front().status);
+}
 
+TEST_F(EspAlarmServiceTest, can_snooze_alarm) {
+    alarms_t alarm;
+    alarm.status = AlarmStatus::Ringing;
+    alarms.setAlarm(alarm);
+    ASSERT_EQ(alarms.getAlarms().size(), 1);
+    ASSERT_EQ(AlarmStatus::Ringing, alarms.getAlarms().front().status);
+    testee.snooze();
+    EXPECT_EQ(AlarmStatus::Snoozed, alarms.getAlarms().front().status);
+}
+
+TEST_F(EspAlarmServiceTest, can_pacify_snoozed_alarm) {
+    alarms_t alarm;
+    alarm.status = AlarmStatus::Snoozed;
+    alarms.setAlarm(alarm);
+    ASSERT_EQ(AlarmStatus::Snoozed, alarms.getAlarms().front().status);
+    testee.pacify();
+    EXPECT_EQ(AlarmStatus::Pacified, alarms.getAlarms().front().status);
 }
