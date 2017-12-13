330e8b5391037e331f1028555651e7d05c1fc365
Simon Egli <simon@egli.online>
2017-12-13 16:13:02 +0100

    Giving EspButton its own thread to detect presses

--- main/EspButton.cpp
+++ main/EspButton.cpp
@@ -1,21 +1,69 @@
 #include "EspButton.h"
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "esp_log.h"
 #include "driver/gpio.h"
+#include <thread>
+
+static const char* TAG = "Button";
+static const unsigned int SHORT_PRESS = 5;
+static const unsigned int LONG_PRESS = 50;
+
+void button_Task(void *pvParameters) {
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+}
+void EspButton::buttonTask(void *pvParameters) {
+    EspButton* button = static_cast<EspButton*>(pvParameters);
+    while(true) {
+        if(gpio_get_level(static_cast<gpio_num_t>(button->gpio_)) ^ button->inverse_) {
+            //ESP_LOGI(TAG, "Button pressed in task");
+            if(button->pressedTime<255) button->pressedTime++;
+        } else {
+            button->pressedTime = 0;
+        }
+
+
+        if(button->pressed() && button->pressCb_)
+            button->pressCb_();
+
+        if(button->longPress() && button->longPressCb_)
+            button->longPressCb_();
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
 
 EspButton::EspButton(unsigned int gpio, bool inverse): gpio_(gpio), inverse_(inverse) {
     gpio_config_t io_conf{};
+    BaseType_t returned{};
     //TODO: will probably overwrite existing button configurations...
     io_conf.pin_bit_mask = (1<<gpio_);
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = static_cast<gpio_pullup_t>(0);
     gpio_config(&io_conf);
+    std::thread task(EspButton::buttonTask, this);
+    task.detach();
+
+    ESP_LOGI(TAG, "Started button task");
+    //returned = xTaskCreate(&button_Task, "buttontask", 4000, NULL, 2, &buttonTaskHandle);
+    //returned = xTaskCreate(&button_Task, "buttontask", 4000, NULL, 2, NULL);
+    /* if(returned != pdPASS) { */
+    /*     ESP_LOGI(TAG, "Error! Could not create button task"); */
+    /* } */
+}
+
+EspButton::~EspButton() {
+    vTaskDelete(buttonTaskHandle);
 }
 
 bool EspButton::pressed() {
-    return gpio_get_level(static_cast<gpio_num_t>(gpio_)) ^ inverse_;
+    //return gpio_get_level(static_cast<gpio_num_t>(gpio_)) ^ inverse_;
+    return pressedTime == SHORT_PRESS;
 
 }
 bool EspButton::longPress() {
-    return true;
+    return pressedTime == LONG_PRESS;
 }
 
 void EspButton::setPressCb(std::function<void()> function) {
--- main/EspButton.h
+++ main/EspButton.h
@@ -2,6 +2,9 @@
 
 #include "Button.h"
 
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #include <functional>
 
 class EspButton : 
@@ -10,14 +13,18 @@ class EspButton :
     public: 
         EspButton() = delete;
         EspButton(unsigned int gpio, bool inverse);
+        ~EspButton();
         virtual bool pressed() override;
         virtual bool longPress() override;
         virtual void setPressCb(std::function<void()>) override;
         virtual void setLongPressCb(std::function<void()>) override;
     private:
-        unsigned int gpio_;
-        bool inverse_;
-        std::function<void()> pressCb_;
-        std::function<void()> longPressCb_;
+        unsigned int gpio_{};
+        bool inverse_{};
+        uint8_t pressedTime{};
+        std::function<void()> pressCb_{};
+        std::function<void()> longPressCb_{};
+        static void buttonTask(void *pvParameters);
+        TaskHandle_t buttonTaskHandle{};
 };
 
