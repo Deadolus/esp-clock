3734ee3589d2942b457d014334ede9b19abef725
Simon Egli <simon@egli.online>
2017-12-15 18:21:53 +0100

    Operator== for alarm

--- main/Alarm.h
+++ main/Alarm.h
@@ -23,6 +23,9 @@ struct alarms_t {
     //!days of week, bit 0 is sunday, bit 6 saturday
     std::bitset<7> weekRepeat{};
     std::function<void(alarms_t&)> callback{};
+    friend bool operator==(const alarms_t& lhs, const alarms_t& rhs) {
+        return std::tie(lhs.time, lhs.name) == std::tie(rhs.time, rhs.name);
+    }
 };
 
 class Alarm 
@@ -31,4 +34,5 @@ class Alarm
         virtual void setAlarm(alarms_t& alarm) = 0;
         virtual std::list<alarms_t>& getAlarms() const = 0;
         virtual alarms_t getNextAlarm() = 0;
+        virtual void deleteAlarm(alarms_t& alarm) = 0;
 };
