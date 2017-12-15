0f4e75006cf281b00f8b51c35aa02316c8e9be6a
Simon Egli <simon@egli.online>
2017-12-15 18:21:18 +0100

    Making AlarmService more bug-free

--- main/EspAlarmService.cpp
+++ main/EspAlarmService.cpp
@@ -16,24 +16,27 @@ bool correctDayOfWeek(std::bitset<7>& days) {
     //tm_wday is days since sundays
     tm now_tm = *localtime(&now);
 
-    //ESP_LOGI(TAG, "Now is %u, comparison: %d, t%d f%d, %s", now_tm.tm_wday, days.test(now_tm.tm_wday), true, false, days.to_string().c_str());
-    return days.test(now_tm.tm_wday);
+    //our bitset is defined as MSB==sunday, LSB==saturday, so mirror here
+    //ESP_LOGI(TAG, "Now is %u, comparison: %d, %s", now_tm.tm_wday, days.test(days.size()-1-now_tm.tm_wday), days.to_string().c_str());
+    return days.test(days.size()-1-now_tm.tm_wday);
 
 }
 bool correctTime(std::chrono::system_clock::time_point alarmTime) {
     tm alarm_tm = Clock::getTm(alarmTime);
     tm now_tm = Clock::getCurrentTimeAsTm();
-    return (now_tm.tm_hour == alarm_tm.tm_hour) && (now_tm.tm_min == alarm_tm.tm_min) && (now_tm.tm_sec == alarm_tm.tm_sec);
+        bool ret = std::tie(now_tm.tm_hour, now_tm.tm_min, now_tm.tm_sec) == std::tie(alarm_tm.tm_hour, alarm_tm.tm_min, alarm_tm.tm_sec);
+    //ESP_LOGI(TAG, "Now is time %d:%d, alarm %d:%d, comp: %d", now_tm.tm_hour, now_tm.tm_min, alarm_tm.tm_hour, alarm_tm.tm_min, ret);
+    //return (now_tm.tm_hour == alarm_tm.tm_hour) && (now_tm.tm_min == alarm_tm.tm_min) && (now_tm.tm_sec == alarm_tm.tm_sec);
+    return ret;
 }
 
 bool alarmShouldRing(alarms_t& alarm, std::chrono::minutes snoozeTime) {
         std::chrono::system_clock::time_point snoozePoint = alarm.snoozeTime+snoozeTime;
-        if( 
-                correctDayOfWeek(alarm.weekRepeat) &&  (
-                correctTime(alarm.time)
+        if(
+                (correctDayOfWeek(alarm.weekRepeat) &&  ( correctTime(alarm.time)))
                 || (Clock::getCurrentTimeAsTimePoint() == snoozePoint)
                 || (alarm.status == AlarmStatus::Ringing)
-          ))
+          )
         {
             return true;
         }
@@ -47,6 +50,7 @@ bool EspAlarmService::checkForAlarm() {
     std::lock_guard<std::mutex> guard(alarmServiceMutex);
     //ESP_LOGI(TAG, "Checking for alarm");
     bool ringing{false};
+
     for(auto& alarm: alarms_.getAlarms())
     {
         //ESP_LOGI(TAG, "Now = %lu, alarm time: %lu, Status: %u", std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()), std::chrono::system_clock::to_time_t(alarm.time), static_cast<unsigned int>(alarm.status));
