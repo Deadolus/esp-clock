d85020d04305cc3655fb5c67e8cd8095bf3beb6f
Simon Egli <deadolus@gmail.com>
2017-12-18 15:18:09 +0100

    Cleanup - removing timer dummy code

--- main/e_ink_diplay_main.cpp
+++ main/e_ink_diplay_main.cpp
@@ -61,33 +61,9 @@
 #include <thread>
 #include <iomanip>
 
-#include "stdlib.h"
-
-/* Can run 'make menuconfig' to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-   */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
-#define TIMER_DIVIDER         16  //  Hardware timer clock divider
-#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
-#define TIMER_INTERVAL0_SEC   (1) // sample test interval for the first timer
-#define TIMER_INTERVAL1_SEC   (5.78)   // sample test interval for the second timer
-#define TEST_WITHOUT_RELOAD   0        // testing will be done without auto reload
-#define TEST_WITH_RELOAD 1 // testing will be done with auto reload
-static unsigned int timer_variable = 0;
-
 static const char *TAG = "clock";
-/* Variable holding number of times ESP32 restarted since first boot.
- * It is placed into RTC memory using RTC_DATA_ATTR and
- * maintains its value when ESP32 wakes from deep sleep.
- */
-//RTC_DATA_ATTR static int boot_count = 0; 
-static void example_tg0_timer_init(timer_idx_t timer_idx, bool auto_reload, double timer_interval_sec);
-static void setTimezone();
-void IRAM_ATTR timer_group0_isr(void *para);
 
-unsigned long time_start_ms;
-unsigned long time_now_s;
-
+void setTimezone();
 
 extern "C" void app_main()
 {
@@ -119,8 +95,6 @@ extern "C" void app_main()
     wifi.startWifi();
     setTimezone();
     sntp.getTime(false);
-    /* if(esp_sleep_get_wakeup_cause() != ESP_SLEEP_WAKEUP_TIMER) */
-    //example_tg0_timer_init(TIMER_0, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
     httpserver.startServer();
     EspDisplayService displayService{display, espsign, alarm, alarms, wifi, sntp, 5000};
     ESP_LOGI(TAG, "Everything started...");
@@ -140,87 +114,9 @@ extern "C" void app_main()
         }
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
-    //esp_sleep_enable_timer_wakeup(0.2 * 1000000);
-    //esp_light_sleep_start();
-    //esp_deep_sleep_start();
 }
 
 void setTimezone() {
 	setenv("TZ", "CET-1", 1);
 	tzset();
-}
-
-/*
- * Initialize selected timer of the timer group 0
- *
- * timer_idx - the timer number to initialize
- * auto_reload - should the timer auto reload on alarm?
- * timer_interval_sec - the interval of alarm to set
- */
-static void example_tg0_timer_init(timer_idx_t timer_idx, 
-        bool auto_reload, double timer_interval_sec)
-{
-    /* Select and initialize basic parameters of the timer */
-    timer_config_t config;
-    config.divider = TIMER_DIVIDER;
-    config.counter_dir = TIMER_COUNT_UP;
-    config.counter_en = TIMER_PAUSE;
-    config.alarm_en = TIMER_ALARM_EN;
-    config.intr_type = TIMER_INTR_LEVEL;
-    config.auto_reload = auto_reload;
-    timer_init(TIMER_GROUP_0, timer_idx, &config);
-
-    /* Timer's counter will initially start from value below.
-       Also, if auto_reload is set, this value will be automatically reload on alarm */
-    timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
-
-    /* Configure the alarm value and the interrupt on alarm. */
-    timer_set_alarm_value(TIMER_GROUP_0, timer_idx, timer_interval_sec * TIMER_SCALE);
-    timer_enable_intr(TIMER_GROUP_0, timer_idx);
-    timer_isr_register(TIMER_GROUP_0, timer_idx, timer_group0_isr, 
-            (void *) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
-
-    timer_start(TIMER_GROUP_0, timer_idx);
-}
-/*
- * Timer group0 ISR handler
- *
- * Note:
- * We don't call the timer API here because they are not declared with IRAM_ATTR.
- * If we're okay with the timer irq not being serviced while SPI flash cache is disabled,
- * we can allocate this interrupt without the ESP_INTR_FLAG_IRAM flag and use the normal API.
- */
-void IRAM_ATTR timer_group0_isr(void *para)
-{
-    //timer_idx_t timer_idx = (int) para;
-    timer_idx_t timer_idx = static_cast<timer_idx_t>((int)para);
-
-    /* Retrieve the interrupt status and the counter value
-       from the timer that reported the interrupt */
-    uint32_t intr_status = TIMERG0.int_st_timers.val;
-    TIMERG0.hw_timer[timer_idx].update = 1;
-    uint64_t timer_counter_value = 
-        ((uint64_t) TIMERG0.hw_timer[timer_idx].cnt_high) << 32
-        | TIMERG0.hw_timer[timer_idx].cnt_low;
-
-    /* Clear the interrupt
-       and update the alarm time for the timer with without reload */
-    if ((intr_status & BIT(timer_idx)) && timer_idx == TIMER_0) {
-        /* evt.type = TEST_WITHOUT_RELOAD; */
-        TIMERG0.int_clr_timers.t0 = 1;
-        timer_counter_value += (uint64_t) (TIMER_INTERVAL0_SEC * TIMER_SCALE);
-        TIMERG0.hw_timer[timer_idx].alarm_high = (uint32_t) (timer_counter_value >> 32);
-        TIMERG0.hw_timer[timer_idx].alarm_low = (uint32_t) timer_counter_value;
-        timer_variable = timer_counter_value;
-    } else if ((intr_status & BIT(timer_idx)) && timer_idx == TIMER_1) {
-        /* evt.type = TEST_WITH_RELOAD; */
-        TIMERG0.int_clr_timers.t1 = 1;
-    } else {
-        /* evt.type = -1; // not supported even type */
-    }
-
-    /* After the alarm has been triggered
-       we need enable it again, so it is triggered the next time */
-    TIMERG0.hw_timer[timer_idx].config.alarm_en = TIMER_ALARM_EN;
-
 }
