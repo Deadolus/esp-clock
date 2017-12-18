81cbcaff0f7dbdc474d6643f18dd31b49f36ebe2
Simon Egli <deadolus@gmail.com>
2017-12-18 15:20:04 +0100

    Cleanup and de-cluttering

--- main/e_ink_diplay_main.cpp
+++ main/e_ink_diplay_main.cpp
@@ -1,29 +1,3 @@
-/**
- *  @filename   :   epd1in54-demo.ino
- *  @brief      :   1.54inch e-paper display demo
- *  @author     :   Yehui from Waveshare
- *
- *  Copyright (C) Waveshare     September 5 2017
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documnetation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to  whom the Software is
- * furished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 #include "EspDisplay.h"
 #include "EspSign.h"
 #include "EspSntpClient.h"
@@ -83,11 +57,11 @@ extern "C" void app_main()
             ESP_LOGI(TAG, "Button pressed!");
             alarms.pacify();
             audioplayer.stopAudio();
-            //display.fullUpdate();
+            display.fullUpdate();
             pwmLed.setIntensity(0);
             if(!wifi.isConnected()) wifi.startWifi();
             });
-    button.setLongPressCb([](){
+    button.setLongPressCb([&](){
             ESP_LOGI(TAG, "Button long pressed!");
             if(wifi.isConnected()) wifi.stopWifi();
             });
@@ -100,8 +74,6 @@ extern "C" void app_main()
     ESP_LOGI(TAG, "Everything started...");
 
     while(true) {
-        //all of the framebuffer is updated on every updateTime
-        //thus no need to call it twice
         alarms_t nextAlarm = alarm.getNextAlarm();
         if(alarms.checkForAlarm()) {
             auto ringingAlarms = alarms.getRingingAlarms();
