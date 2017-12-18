a3d1f3b6a729997f5c48cfcd44d9572291d5b51d
Simon Egli <deadolus@gmail.com>
2017-12-18 13:10:09 +0100

    Faking __UCLIBC__ to get to_string()
    Hack - should probably work some other way

--- main/component.mk
+++ main/component.mk
@@ -3,3 +3,5 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 COMPONENT_ADD_INCLUDEDIRS += ../libraries ../httpd
+#CXXFLAGS += -std=c++14 -D__UCLIBC__
+CXXFLAGS += -D__UCLIBC__
