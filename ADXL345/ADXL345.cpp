77afc3a3709d9ccc2e7f0f44bb31e6661f8d5130
Simon Egli <deadolus@gmail.com>
2018-01-09 15:46:43 +0100

    Adding default I2C esp32 initalization

--- ADXL345/ADXL345.cpp
+++ ADXL345/ADXL345.cpp
@@ -82,6 +82,24 @@ ADXL345::ADXL345(uint8_t address) {
  * less demanding mode of operation.
  */
 void ADXL345::initialize() {
+static gpio_num_t I2C_EXAMPLE_MASTER_SCL_IO{static_cast<gpio_num_t>(19)};               /*!< gpio number for I2C master clock */
+static gpio_num_t I2C_EXAMPLE_MASTER_SDA_IO{static_cast<gpio_num_t>(18)};               /*!< gpio number for I2C master data  */
+#define I2C_EXAMPLE_MASTER_NUM             I2C_NUM_1        /*!< I2C port number for master dev */
+#define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
+#define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
+#define I2C_EXAMPLE_MASTER_FREQ_HZ         100000           /*!< I2C master clock frequency */
+    i2c_port_t i2c_master_port = I2C_EXAMPLE_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_EXAMPLE_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_EXAMPLE_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_EXAMPLE_MASTER_FREQ_HZ;
+    i2c_param_config(i2c_master_port, &conf);
+    i2c_driver_install(i2c_master_port, conf.mode,
+                       I2C_EXAMPLE_MASTER_RX_BUF_DISABLE,
+                       I2C_EXAMPLE_MASTER_TX_BUF_DISABLE, 0);
     writeByte(devAddr, ADXL345_RA_POWER_CTL, 0); // reset all power settings
     setAutoSleepEnabled(true);
     setMeasureEnabled(true);
