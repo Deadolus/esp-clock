289921e002687f14cd65d188cc5478c6547284e2
Simon Egli <deadolus@gmail.com>
2018-01-16 15:09:58 +0100

    Making ADXL345 library compilable

--- ADXL345/ADXL345.cpp
+++ ADXL345/ADXL345.cpp
@@ -37,8 +37,8 @@ THE SOFTWARE.
 #define READ_BIT                           I2C_MASTER_READ  /*!< I2C master read */
 #define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
 #define ACK_CHECK_DIS                      0x0              /*!< I2C master will not check ack from slave */
-#define ACK_VAL                            0x0              /*!< I2C ack value */
-#define NACK_VAL                           0x1              /*!< I2C nack value */
+static const i2c_ack_type_t ACK_VAL{static_cast<i2c_ack_type_t>(0x0)};              /*!< I2C ack value */
+static const i2c_ack_type_t NACK_VAL{static_cast<i2c_ack_type_t>(0x1)};              /*!< I2C nack value */
 
 static i2c_port_t i2c_num =               I2C_NUM_0;        /*!<I2C port number for slave dev */
 
