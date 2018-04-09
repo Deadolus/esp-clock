2599de6ffaa6ea2bc1b8570fd0a792690d607008
Simon Egli <simon@egli.online>
2018-04-09 21:22:17 +0200

    Adding forgotten while loop to sensor service task

--- ADXL345/ADXLService.cpp
+++ ADXL345/ADXLService.cpp
@@ -13,6 +13,7 @@ ADXLService::ADXLService() {
     sensor_.initialize();
     sensor_.setMeasureEnabled(true);
     sensor_.setSleepEnabled(false);
+    sensor_.setRange(0);
     ESP_LOGI(TAG, "Test sensor %d", sensor_.testConnection());
 #ifndef GOOGLETEST
     std::thread task(ADXLServiceTask, std::ref(sensor_));
@@ -22,6 +23,8 @@ ADXLService::ADXLService() {
 
 void ADXLService::ADXLServiceTask(ADXL345& sensor) {
 
-    ESP_LOGI(TAG, "Accel: %d, %d, %d", sensor.getAccelerationX(), sensor.getAccelerationY(), sensor.getAccelerationZ());
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    while(true) {
+        ESP_LOGI(TAG, "Accel: %d, %d, %d", sensor.getAccelerationX(), sensor.getAccelerationY(), sensor.getAccelerationZ());
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 }
