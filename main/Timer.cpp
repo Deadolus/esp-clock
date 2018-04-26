9d7f9e0f278127d16642355f50d03db61c2aebf7
Simon Egli <simon.egli@u-blox.com>
2018-04-26 14:12:59 +0200

    Fleshing out timer

--- main/Timer.cpp
+++ main/Timer.cpp
@@ -1,8 +1,18 @@
-#pragma once
 #include "Timer.h"
 #include <chrono>
 #include <functional>
+#include <thread>
+
+namespace {
+    void timerTask(std::chrono::milliseconds time, std::function<void()>& callback)
+    {
+        std::this_thread::sleep_for(time);
+        if(callback)
+            callback();
+    }
+}
 
 Timer::Timer(std::chrono::milliseconds time, std::function<void()>& callback) {
-
+    std::thread task(timerTask, time, std::ref(callback));
+    task.detach();
 }
--- unittests/tests/EspTimerTest.cpp
+++ unittests/tests/EspTimerTest.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 #include "Timer.h"
 #include "Clock.h"
+#include <chrono>
+#include <thread>
+#include <mutex>
 
 class TimerTest : public ::testing::Test
 {
@@ -9,6 +12,29 @@ class TimerTest : public ::testing::Test
 
 TEST_F(TimerTest, can_construct_timer)
 {
-    std::function<void()> lambda = []()->void{};
-    Timer testee{std::chrono::milliseconds(5), lambda};
+    bool test{false};
+    std::function<void()> lambda = [&]()->void{test=true;};
+    auto time = std::chrono::milliseconds(5);
+    Timer testee{time, lambda};
+    std::this_thread::sleep_for(time+std::chrono::milliseconds(1));
+
+    EXPECT_TRUE(test);
+}
+
+TEST_F(TimerTest, does_sleep_for_appropriate_time)
+{
+    bool test{false};
+    std::mutex lock;
+    lock.lock();
+    std::function<void()> lambda = [&]()->void{lock.unlock();};
+    auto time = std::chrono::milliseconds(5);
+    auto timeBefore=Clock::getCurrentTimeAsTimePoint();
+    Timer testee{time, lambda};
+    //will block until timer unlocks mutex
+    lock.lock();
+    auto timeAfter=Clock::getCurrentTimeAsTimePoint();
+    auto duration = timeAfter - timeBefore;
+
+    EXPECT_GT(duration, time);
+    EXPECT_LT(duration, 1.1*time);
 }
