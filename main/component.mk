50fa89f4afd11e82c0cb18ebf7ce9f33e94d873a
Simon Egli <git@egli.online>
2019-05-06 19:01:38 +0200

    Disabling cxxflag

--- main/component.mk
+++ main/component.mk
@@ -4,4 +4,4 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 COMPONENT_ADD_INCLUDEDIRS += ../libraries ../httpd ../ADXL345
 #CXXFLAGS += -std=c++14 -D__UCLIBC__
-CXXFLAGS += -D__UCLIBC__
+#CXXFLAGS += -D__UCLIBC__
